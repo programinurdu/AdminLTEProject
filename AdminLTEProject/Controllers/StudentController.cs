e27d16fba2f6b9717892411dfd4b587b35888ed5
Admin <eaak2004@hotmail.com>
2019-08-31 19:07:54 +0100

    X

---
+++ AdminLTEProject/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminLTEProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult StudentsList()
+        {
+            return View();
+        }
+
+        public ActionResult StudentInfo()
+        {
+            return View();
+        }
+    }
+}
